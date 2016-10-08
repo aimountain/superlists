832ee7bb810c73f4b50a94819de2ba4df88d1669
aimountain <aimountain@126.com>
2016-10-08 14:12:06 +0800

    add functional

--- functional_tests.py
+++ functional_tests.py
@@ -45,9 +45,12 @@ class NewVistorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any( row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
+                any( row.text == '1:Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table  its test was: \n%s" % (table.text)
             )
+
+        self.assertIn('2: Use peacock feathers to make a fly', [ row.text for row in rows])
+        
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 “ Use peacock feathers to make a fly ” （使用孔雀羽毛做假蝇）
