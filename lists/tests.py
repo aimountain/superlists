8a352fe00c0e5e0ed8c6cd664690be96c8d4c03d
aimountain <aimountain@126.com>
2016-10-09 14:23:49 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -23,15 +23,45 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_can_save_a_POST_request(self):
+
         request  = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'a new list item'
 
         response = home_page(request)
-        self.assertIn('a new list item' , response.content.decode())
-        expected_html = render_to_string('home.html', {'new_item_text': 'a new list item'})
-        self.assertEqual(response.content.decode(), expected_html)
         
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'a new list item')
+
+
+    def test_home_page_redirects_after_POST(self):
+        request  = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'a new list item'
+
+        response = home_page(request)
+        
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_home_page_displays_all_items(self):
+        Item.objects.create(text='itemy 1')
+        Item.objects.create(text='itemy 2')
+        
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemy 1', response.content.decode())
+        self.assertIn('itemy 2', response.content.decode())
+
 
 
 class ItemModelTest(TestCase):
