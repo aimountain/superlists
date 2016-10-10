04023e282134007636db8da5ce57af745481bbab
aimountain <aimountain@126.com>
2016-10-10 17:24:27 +0800

    add functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,25 +44,58 @@ class NewVistorTest(LiveServerTestCase):
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 她按回车键后，页面更新了
+        # 她按回车键后，被带到了一个新URL
         # 待办事项表格中显示了 “ 1: Buy peacock feathers ”
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any( row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table  its test was: \n%s" % (table.text)
-            )
-
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了 “ Use peacock feathers to make a fly ” （使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
+
+        inputbox.send_keys('Use peacock feathers to make a fly')
+ 
+        inputbox.send_keys(Keys.ENTER)
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-       
+
+        # 现在一个叫作弗朗西斯的新用户访问了网站
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来 # ➊
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_ur
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        # 两人都很满意，去睡觉了
+
         self.fail('Finish the test!')
 
-        # 页面再次更新，她的清单中显示了这两个待办事项
+        # 她访问那个URL，发现她的待办事项清单还在
+        # 她很满意，去睡觉了
+
+
         
 
